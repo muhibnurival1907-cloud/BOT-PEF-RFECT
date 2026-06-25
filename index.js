8a92514c44b2d92da055076a6e3d11dcbe348f64
Muhib Nuri Val <muhibnurival1907@gmail.com>
2026-06-25 21:39:56 +0700

    Fix yt-dlp path and add nixpacks

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const os = require('os');
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 const isWindows = os.platform() === 'win32';
-const ytDlpPath = isWindows ? path.join(__dirname, 'yt-dlp.exe') : '/usr/local/bin/yt-dlp';
+// Jika di Linux/Railway, cukup gunakan 'yt-dlp' agar sistem mencarinya di PATH
+const ytDlpPath = isWindows ? path.join(__dirname, 'yt-dlp.exe') : 'yt-dlp'; : '/usr/local/bin/yt-dlp';
 const ffmpegPath = isWindows ? path.join(__dirname, 'ffmpeg.exe') : 'ffmpeg';
 const folderDownloads = path.join(__dirname, 'downloads');
 
